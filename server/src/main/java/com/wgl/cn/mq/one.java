03d1c8c370fb80004df4361ad57a7d1a57f282c6
wuguilin <wuguilin@4366.com>
2020-07-10 21:02:36 +0800

    avtive mq

---
+++ server/src/main/java/com/wgl/cn/mq/one.java
@@ -0,0 +1,15 @@
+package com.wgl.cn.mq;
+
+/**
+ * @Description TODO
+ * @Author guilin wu
+ * @Date 2020-07-10 001021:01:46
+ * #Version 1.0
+ **/
+
+public class one {
+}
+
+https://blog.csdn.net/a616565539/article/details/79674855?utm_medium=distribute.pc_relevant.none-task-blog-BlogCommendFromMachineLearnPai2-3.nonecase&depth_1-utm_source=distribute.pc_relevant.none-task-blog-BlogCommendFromMachineLearnPai2-3.nonecase
+
+
